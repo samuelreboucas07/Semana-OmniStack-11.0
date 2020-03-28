4be973baa6c85488925c1dad7425b9e373c3fe77
Samuel Rebouças de Jesus <samuelreboucas07@hotmail.com>
2020-03-28 19:51:16 -0300

    Backend aplication

---
+++ Backend/src/database/migrations/20200328174005_create_ongs.js
@@ -0,0 +1,17 @@
+ 
+//Responsável pela criação da tabela.
+exports.up = function(knex) {
+  return knex.schema.createTable('ongs', function(table) {
+      table.string('id').primary();
+      table.string('name').notNullable();
+      table.string('email').notNullable();
+      table.string('whatsapp').notNullable();
+      table.string('city').notNullable();
+      table.string('uf', 2).notNullable();
+  })
+};
+
+//O que é necessário ser realizado caso ocorra um erro na criação da tabela. 
+exports.down = function(knex) {
+  return knex.schema.dropTable('ongs');
+};
